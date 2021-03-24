1cf252c185efc63568859bdbcb06d0e6eb61a02e
Chantell Chapman <chantellochapman@gmail.com>
2021-03-23 19:36:56 -0700

    managed API requests

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 //Selectors
 const ballSelector = document.querySelector('#ball');
+const buttonSelector = document.querySelector('#button');
+const inputSelector = document.querySelector('#input');
+const answerSelector = document.querySelector('#answer');
 
 //API 
 const API_ENDPOINT = 'https://yesno.wtf/api';
  
+ //Flags
+ let isRequestInProgress = false;
 
 //Output API's response
 const showAnswer = answer => {
 	setTimeout(() => {
-		document.querySelector('#answer').innerHTML = `<p>${answer}</p>`;
+		answerSelector.innerHTML = `<p>${answer}</p>`;
 		ballSelector.classList.remove('shake-ball');
 		cleanupResponse();
 
@@ -18,6 +23,9 @@ const showAnswer = answer => {
 
 //fetchAnswer function and call API 
 const fetchAnswer = () => {
+	setIsRequestInProgress(true); //isRequestInProgress = true;
+
+	setDisableButtonState(true);
 	ballSelector.classList.add('shake-ball');
 
 	fetch(API_ENDPOINT)
@@ -25,30 +33,54 @@ const fetchAnswer = () => {
 		.then(data => showAnswer(data.answer));
 };
 
-
-//Attach fetchAnswer to event listener
-const handleKeyEnter = (e) => {
-	//console.log('handleKeyEnter', {e});
-	if (e.keyCode === 13) {
-		fetchAnswer();
-	}	 
-
-};
-
-document.querySelector('#button').addEventListener('click', () => {
-	//console.log('Clicked');
-	fetchAnswer();
-});
-
 //Clear output after 3 seconds
 const cleanupResponse = () => {
 	setTimeout(() => {
-		document.querySelector('#answer').innerHTML = '';
-		document.querySelector('#input').value = '';
-
+		answerSelector.innerHTML = '';
+		inputSelector.value = '';
+		setIsRequestInProgress(false); //isRequestInProgress = false;
+		setDisableButtonState(false);
 	}, 3000);
 	
 
 };
+
+//Manage API requests
+const setDisableButtonState = (isDisabling) => {
+	if (isDisabling) {
+		buttonSelector.setAttribute('disabled', 'disabled');
+	} else {
+		buttonSelector.removeAttribute('disabled');
+
+	}
+	
+};
+
+const setIsRequestInProgress = (value) => {
+	isRequestInProgress = value;
+};
+
+const getAnswer = () => {
+	if (isRequestInProgress) {
+		return;
+	}
+
+	if (!inputSelector.value) {
+		return
+	}
+
+	fetchAnswer();
+};
+
+//Attach fetchAnswer to event listener
+const handleKeyEnter = (e) => {
+	if (e.keyCode === 13) {
+		getAnswer();
+	}	 
+
+};
+
+buttonSelector.addEventListener('click', getAnswer); 
+
 
 //Add loading/error states
